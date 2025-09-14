3805dd7d4cd55ce7329ce9ee0718c1a26aed5cdc
ZrozumiloStudio <zrozumilostudio@gmail.com>
2025-09-14 19:38:33 +0300

    Update
    Update

--- launch.bat
+++ launch.bat
@@ -1,22 +1,3 @@
 @echo off
-SETLOCAL
-
-python -m ensurepip --upgrade
-python -m pip install --upgrade pip
-
-IF EXIST requirements.txt (
-    echo Installing libraries...
-    python -m pip install -r requirements.txt
-) ELSE (
-    echo requirements.txt not found
-)
-
-IF EXIST !Main.py (
-    echo Launching...
-    python !main.py
-) ELSE (
-    echo damn
-)
-
-echo Ready
-pause
+REM ...
+start "" "%~dp0Trilogy/!Main.exe"
